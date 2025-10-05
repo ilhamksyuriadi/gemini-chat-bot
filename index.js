16049100ad0472dff2f79130d47f0c72c3397e3c
ilham <ilhamksyuriadi@gmail.com>
2025-10-05 16:01:01 +0700

    simple chatbot

---
+++ index.js
@@ -0,0 +1,84 @@
+import express from 'express';
+import cors from 'cors';
+import { GoogleGenAI } from '@google/genai';
+
+import 'dotenv/config';
+
+const app = express();
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+
+app.post('/chat', async (req, res) => {
+  const { body } = req;
+  const { conversation } = body;
+
+  if (!conversation || !Array.isArray(conversation)) {
+    res.status(400).json({
+      message: "Bad reqest",
+      data: null,
+      success: false
+    });
+    return;
+  }
+
+  const conversationIsValid = conversation.every((message) => {
+    if (!message) return false;
+
+    if (typeof message !== 'object' || Array.isArray(message)) return false;
+
+    const keys = Object.keys(message);
+    const keyLengthIsValid = keys.length === 2;
+    const keyContainsValidName = keys.every(key => ['role', 'text'].includes(key));
+
+    if (!keyLengthIsValid || !keyContainsValidName) return false;
+
+    const { role, text } = message;
+    const roleIsValid = ['user', 'model'].includes(role);
+    const textIsValid = typeof text === 'string';
+
+    if (!roleIsValid || !textIsValid) return false;
+    return true;
+  });
+
+  if (!conversationIsValid) {
+    res.status(400).json({
+      message: "Percakapan harus valid!",
+      data: null,
+      success: false
+    });
+    return;
+  }
+
+  const contents = conversation.map(({ role, text }) => ({
+    role,
+    parts: [{ text }]
+  }));
+
+  try {
+    const aiResponse = await ai.models.generateContent({
+      model: 'gemini-2.5-flash',
+      contents
+    });
+
+    res.status(200).json({
+      success: true,
+      data: aiResponse.text,
+      message: "Success"
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      success: false,
+      data: null,
+      message: e.message || "System error"
+    })
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Running on http://localhost:${PORT}`);
+});
